b8901e489322c4b119fd84308b8b48c6c031c6c0
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2019-05-27 23:15:23 -0400

    Finished laws & cleaned up script

--- ck2_save_cleaner.bash
+++ ck2_save_cleaner.bash
@@ -35,66 +35,83 @@ echo "$HEADER: Removing empty strings..."
 regex 's/  ^.*""  //gmx'  ## No 's'
 
 echo "$HEADER: Removing 'noreligion'..."
-regex 's/  \t*rel(|igion)="noreligion"\n  //gmsx'
+regex 's/  \t*       rel(|igion)="noreligion"\n  //gmsx'
 #regex 's/  \t*(original_|)parent="noreligion"\n  //gmsx'
 
 #echo "$HEADER: Removing 'was_heresy'..."
-#regex 's/  \t*was_heresy=(yes|no)\n  //gmsx'
+regex 's/  \t*was_heresy=(yes|no)\n  //gmsx'
 
-echo "$HEADER: Removing technology..." #NOTE:  Only use with mods that disable tech!
+echo "$HEADER: Removing technology..." #NOTE:  Only for use with mods that don't use tech!
 regex 's/  \t{3}technology=.*?\t{3}\}\n  //gmsx'
 
 ## -----------------------------------------------------------------------------
 HEADER='Characters'
 
 echo "$HEADER: Removing 'hist'..." ## This is just whether a character is historical.  Not really important.
-regex 's/  \t*hist=.*?\n  //gmsx'
+regex 's/            \t* hist=[^\n\t]*?  \n  //gmsx'
 
 #echo "$HEADER: Removing 'pi'..."  ## This is whether a character is ???.  Probably not important?
-#regex 's/  \t*pi=.*?\n   //gmsx'
+#regex 's/            \t*   pi=[^\n\t]*?  \n  //gmsx'
 
 echo "$HEADER: Removing stats from the deceased..."
-regex 's/  d_d.*?\K  \t{3}att=.*?\}\n  //gmsx'
+regex 's/  d_d.*?\K  \t{3}att=[^\n\t]*?\}\n  //gmsx'
 
 ## -----------------------------------------------------------------------------
 HEADER='Inactive titles'
 
 echo "$HEADER: Removing laws..."
-#regex 's/\t{3}law=.*?\n              (?=.*active=no)//gmsx'
-regex 's/  active=no.*?\K  \t{3}council_voting=.*?\}\n          (?=.*\t{2}\})  //gmsx'
+regex 's/                  \t{3}                 succession= [^\n\t]*?  \n  (?=  (?:\t{3,}[^\n\t]*?\n)*?  \t{3}active=no)  //gmsx'
+regex 's/                  \t{3}                     gender= [^\n\t]*?  \n  (?=  (?:\t{3,}[^\n\t]*?\n)*?  \t{3}active=no)  //gmsx'
+regex 's/                  \t{3}                        law= [^\n\t]*?  \n  (?=  (?:\t{3,}[^\n\t]*?\n)*?  \t{3}active=no)  //gmsx'
+regex 's/                  \t{3} set_protected_inheritance = [^\n\t]*?  \n  (?=  (?:\t{3,}[^\n\t]*?\n)*?  \t{3}active=no)  //gmsx'
+regex 's/                  \t{3} set_appoint_generals      = [^\n\t]*?  \n  (?=  (?:\t{3,}[^\n\t]*?\n)*?  \t{3}active=no)  //gmsx'
+regex 's/                  \t{3} set_allow_title_revokation= [^\n\t]*?  \n  (?=  (?:\t{3,}[^\n\t]*?\n)*?  \t{3}active=no)  //gmsx'
+regex 's/                  \t{3}allows_matrilineal_marriage= [^\n\t]*?  \n  (?=  (?:\t{3,}[^\n\t]*?\n)*?  \t{3}active=no)  //gmsx'
+regex 's/                  \t{3} set_investiture           = [^\n\t]*?  \n  (?=  (?:\t{3,}[^\n\t]*?\n)*?  \t{3}active=no)  //gmsx'
+regex 's/  active=no.*?\K  \t{3}             council_voting=.*?\n\t{3}\}\n  (?=   .*\t{2}\})                               //gmsx'
 
 echo "$HEADER: Removing dates and timeouts..."
-regex 's/  active=no.*?\K  \t{3}law_vote_date=.*?\n             (?=.*\t{2}\})  //gmsx'
-regex 's/  active=no.*?\K  \t{3}law_change_timeout=.*?\n        (?=.*\t{2}\})  //gmsx'
-regex 's/  active=no.*?\K  \t{3}crown_law_change_timeout=.*?\n  (?=.*\t{2}\})  //gmsx'
+regex 's/                  \t{3}           succ_law_changer= [^\n\t]*?  \n  (?=  (?:\t{3,}[^\n\t]*?\n)*?  \t{3}active=no)  //gmsx'
+regex 's/                  \t{3}        de_jure_law_changer= [^\n\t]*?  \n  (?=  (?:\t{3,}[^\n\t]*?\n)*?  \t{3}active=no)  //gmsx'
+regex 's/                  \t{3}        de_jure_law_change = [^\n\t]*?  \n  (?=  (?:\t{3,}[^\n\t]*?\n)*?  \t{3}active=no)  //gmsx'
+regex 's/                  \t{3}        de_jure_law_changes= [^\n\t]*?  \n  (?=  (?:\t{3,}[^\n\t]*?\n)*?  \t{3}active=no)  //gmsx'
+regex 's/                  \t{3}         normal_law_changer= [^\n\t]*?  \n  (?=  (?:\t{3,}[^\n\t]*?\n)*?  \t{3}active=no)  //gmsx'
+regex 's/                  \t{3}         normal_law_change = [^\n\t]*?  \n  (?=  (?:\t{3,}[^\n\t]*?\n)*?  \t{3}active=no)  //gmsx'
+regex 's/                  \t{3}               last_change = [^\n\t]*?  \n  (?=  (?:\t{3,}[^\n\t]*?\n)*?  \t{3}active=no)  //gmsx'
+regex 's/  active=no.*?\K  \t{3}              law_vote_date= [^\n\t]*?  \n  (?=   .*\t{2}\})                               //gmsx'
+regex 's/  active=no.*?\K  \t{3}         law_change_timeout= [^\n\t]*?  \n  (?=   .*\t{2}\})                               //gmsx'
+regex 's/  active=no.*?\K  \t{3}   crown_law_change_timeout= [^\n\t]*?  \n  (?=   .*\t{2}\})                               //gmsx'
 
 echo "$HEADER: Removing misc..."
-#regex 's/\t{3}holding_dynasty=.*?\n  (?=.*active=no)//gmsx'
-regex 's/  active=no.*?\K  \t{3}conquest_culture=.*?\n          (?=.*\t{2}\})  //gmsx'
-regex 's/  active=no.*?\K  \t{3}vars=.*?\}\n                    (?=.*\t{2}\})  //gmsx'
-regex 's/  active=no.*?\K  \t{3}et=.*?\n                        (?=.*\t{2}\})  //gmsx'
+regex 's/                  \t{3}            holding_dynasty= [^\n\t]*?  \n  (?=  (?:\t{3,}[^\n\t]*?\n)*?  \t{3}active=no)  //gmsx'
+regex 's/  active=no.*?\K  \t{3}           conquest_culture= [^\n\t]*?  \n  (?=   .*\t{2}\})                               //gmsx'
+regex 's/  active=no.*?\K  \t{3}                       vars=.*?\n\t{3}\}\n  (?=   .*\t{2}\})                               //gmsx'
+regex 's/  active=no.*?\K  \t{3}                         et= [^\n\t]*?  \n  (?=   .*\t{2}\})                               //gmsx'
 
 ## -----------------------------------------------------------------------------
 HEADER='Diseases'
 
-echo "$HEADER: Wiping general outbreak history..." ## Possible to target only inactive diseases
-regex 's/\n\t{1}  disease_outbreak=.*?  \n\t{1}\}  //gmsx'
+echo "$HEADER: Wiping outbreak timeouts..."
+regex 's/  \n\t{1}dont_break_out_again_entity=.*?\n\t{1}\}  //gmsx'
+
+echo "$HEADER: Wiping general outbreak history..."    ## Possible to target only inactive diseases
+regex 's/  \n\t{1}   disease_outbreak        =.*?\n\t{1}\}  //gmsx'
 
 echo "$HEADER: Wiping provincial outbreak history..." ## Not possible to target only inactive diseases
-regex 's/\n\t{3}  disease=.*?  \n\t{3}\}  //gmsx'
-
-echo "$HEADER: Wiping outbreak timeouts..."
-regex 's/\n\t{1}  dont_break_out_again_entity=.*?  \n\t{1}\}  //gmsx'
+regex 's/  \n\t{3}   disease                 =.*?\n\t{3}\}  //gmsx'
 
 ## -----------------------------------------------------------------------------
 HEADER='Minification'
 
 echo "$HEADER: Removing tabs..."
-regex 's/  \t    //gmsx'
+regex 's/  \t        //gmsx'
 
 echo "$HEADER: Removing unnecessary newlines..."
-regex 's/  \n\{  /\{/gmsx'
-regex 's/  \n\n  /\n/gmsx'
+regex 's/  \n{2,}  /\n/gmsx'
+regex 's/  \n\{    /\{/gmsx'
+
+echo "$HEADER: Condensing spaces..."
+regex 's/  \ {2,}   / /gmsx'
 
 ## -----------------------------------------------------------------------------
 echo "Saving cleaned file..."
